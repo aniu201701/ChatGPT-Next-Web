3904e810438b543a60963683381590c51c942b75
Pino Ou <zf.ou@aftership.com>
2023-11-12 11:32:35 +0800

    :sparkles:Integrate meticulous AI testing capabilities

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-sync-scripts */
 /* eslint-disable @next/next/no-page-custom-font */
 import "./styles/globals.scss";
 import "./styles/markdown.scss";
@@ -23,14 +24,25 @@ export const metadata: Metadata = {
   },
 };
 
+// 20% of the time, enable meticulous
+const isEnableMeticulous =
+  process.env.NODE_ENV !== "production" || Math.random() < 0.2;
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const MeticulousScript = isEnableMeticulous ? (
+    <script
+      data-project-id="hcpL0HiNYUgo4WVdtXUXNWTqQyCFWV9O4Ps1CSxw"
+      src="https://snippet.meticulous.ai/v1/meticulous.js"
+    />
+  ) : null;
   return (
     <html lang="en">
       <head>
+        {MeticulousScript}
         <meta name="config" content={JSON.stringify(getClientConfig())} />
         <link rel="manifest" href="/site.webmanifest"></link>
         <script src="/serviceWorkerRegister.js" defer></script>
